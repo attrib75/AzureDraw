7250e90e278c337f1ad47ddbe63d6a7f4b51c2da
pgoode <pgoode75@sbcglobal.net>
2020-04-14 22:31:57 -0500

    Lots of UI

---
+++ AzureDraw/DAL/VisionServiceClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training.Models;
+
+namespace AzureDraw.DAL
+{
+    public class VisionServiceClient
+    {
+        private string _apiKey;
+        private string _apiEndpoint;
+        private Guid _projectId;
+        private string _modelName;
+
+        public VisionServiceClient(string apiKey, string apiEndpoint, Guid projectId, string modelName)
+        {
+            _apiKey = apiKey;
+            _apiEndpoint = apiEndpoint;
+            _projectId = projectId;
+            _modelName = modelName;
+        }
+
+        public async Task<bool> UploadTrainingImage(MemoryStream memoryStream, string tagName)
+        {
+            CustomVisionTrainingClient trainingApi = new CustomVisionTrainingClient()
+            {
+                ApiKey = _apiKey,
+                Endpoint = _apiEndpoint
+            };
+            var tags = await trainingApi.GetTagsAsync(_projectId);
+            foreach (var tag in tags)
+            {
+                if (tag.Description.Equals(tagName, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    trainingApi.CreateImagesFromData(_projectId, memoryStream, new List<Guid>() { tag.Id });
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
